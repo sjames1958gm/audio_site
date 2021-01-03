7dded5104f758723d20a2e7769e48fc778372fd0
Stephen James <sajames1958@gmail.com>
2021-01-03 11:14:05 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteMetadata: {
+    title: "audio_site",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken:
+          "19a17add415fafe92e08f6a04e95573780bd37ac674a4476f8e8d25b780210d1",
+        spaceId: "ji85scmsrspw",
+      },
+    },
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
